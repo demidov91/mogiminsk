14b5817c75c3fa85a5b59c852d2fc2565952d65e
dmitry <demidov91@mail.ru>
2017-08-15 22:50:02 +0300

    Add providers contacts.

--- mogiminsk/management/fill_contacts.py
+++ mogiminsk/management/fill_contacts.py
@@ -4,25 +4,26 @@ import logging
 from sqlalchemy import and_
 
 from mogiminsk.models import Provider, ProviderContact
-from mogiminsk.utils import Session, configure_session
+from mogiminsk.utils import threaded_session
 
 
 logger = logging.getLogger(__name__)
-db = None
 
 
 @lru_cache(maxsize=10)
 def get_provider(identifier):
-    return db.query(Provider).filter(Provider.identifier == identifier).first()
+    return threaded_session().query(Provider).filter(Provider.identifier == identifier).first()
+
 
 def find_contact(provider, kind, value):
-    return db.query(ProviderContact, Provider).filter(
+    return threaded_session.query(ProviderContact, Provider).filter(
         and_(
             Provider.identifier == provider,
             ProviderContact.kind == kind,
             ProviderContact.contact == value
         )
     ).first()
+
 
 def build_contact(provider, kind, value) -> ProviderContact:
     provider_instance = get_provider(provider)
@@ -47,10 +48,34 @@ def run():
         {'provider': 'dve_stolicy', 'kind': 'mts', 'value': '+375336024444'},
         {'provider': 'dve_stolicy', 'kind': 'life', 'value': '+375256024444'},
         {'provider': 'dve_stolicy', 'kind': 'web', 'value': 'https://2stolict.com'},
+
+        {'provider': 'grand_express', 'kind': 'velcom', 'value': '+375291602222'},
+        {'provider': 'grand_express', 'kind': 'velcom', 'value': '+375291612222'},
+        {'provider': 'grand_express', 'kind': 'mts', 'value': '+375295462222'},
+        {'provider': 'grand_express', 'kind': 'mts', 'value': '+375295452222'},
+        {'provider': 'grand_express', 'kind': 'life', 'value': '+375255102222'},
+        {'provider': 'grand_express', 'kind': 'web', 'value': 'http://grandexpress.by'},
+
+        {'provider': 'stolica_plus', 'kind': 'velcom', 'value': '+375293522215'},
+        {'provider': 'stolica_plus', 'kind': 'mts', 'value': '+375333522215'},
+        {'provider': 'stolica_plus', 'kind': 'life', 'value': '+375259560707'},
+        {'provider': 'stolica_plus', 'kind': 'web', 'value': 'https://m4minsk.by'},
+
+        {'provider': 'minsk_express', 'kind': 'velcom', 'value': '+375447885533'},
+        {'provider': 'minsk_express', 'kind': 'mts', 'value': '+375297885533'},
+        {'provider': 'minsk_express', 'kind': 'life', 'value': '+375257885533'},
+        {'provider': 'minsk_express', 'kind': 'velcom', 'value': '+375447886633'},
+        {'provider': 'minsk_express', 'kind': 'mts', 'value': '+375297886633'},
+        {'provider': 'minsk_express', 'kind': 'life', 'value': '+375257886633'},
+        {'provider': 'minsk_express', 'kind': 'web', 'value': 'http://mogilevminsk.by'},
+
+        {'provider': 'avtoslava', 'kind': 'velcom', 'value': '+375445555161'},
+        {'provider': 'avtoslava', 'kind': 'mts', 'value': '+375295555161'},
+        {'provider': 'avtoslava', 'kind': 'life', 'value': '+375256842235'},
+        {'provider': 'avtoslava', 'kind': 'web', 'value': 'http://avto-slava.by'},
     ]
 
-    global db
-    db = Session()
+    db = threaded_session()
 
     try:
         for contact in contacts:
