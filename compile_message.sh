507a5a9ed42dbc466072dfbe4c735571a2f433f2
dzmitry <demidov91@mail.ru>
2017-10-14 00:19:33 +0300

    Simplify message compile commands.

--- compile_message.sh
+++ compile_message.sh
@@ -1,2 +1,2 @@
-pybabel compile -i  locale/ru/LC_MESSAGES/messages.po -o locale/ru/LC_MESSAGES/messages.mo
-pybabel compile -i  locale/be/LC_MESSAGES/messages.po -o locale/be/LC_MESSAGES/messages.mo
+pybabel compile -d locale -D messages -l ru
+pybabel compile -d locale -D messages -l be
