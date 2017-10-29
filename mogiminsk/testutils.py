0f3fe476118e81435d0bdcb9e4635497deb9e022
dzmitry <demidov91@mail.ru>
2017-10-29 18:40:49 +0300

    More testutils is moved into the main repo.

--- mogiminsk/testutils.py
+++ mogiminsk/testutils.py
@@ -1,4 +1,6 @@
 import asyncio
+import os
+
 from mogiminsk.utils import threaded_session, set_db
 
 
@@ -17,3 +19,29 @@ class DbTest:
             set_db(None)
 
         asyncio.get_event_loop().run_until_complete(local_session_initilizer())
+
+
+def read_file(current_file: str, filename: str) ->bytes:
+    """
+    :param current_file: usually you have to provide __file__ var here.
+    :param filename: path related to the :current_file:
+    :return: Bytes of the file :filename: related to the :current_file:.
+    """
+    base_folder = os.path.dirname(current_file)
+    with open(os.path.join(base_folder, filename), mode='rb') as f:
+        return f.read()
+
+
+class MockedAioHttpClient:
+    def __init__(self, response):
+        class Response:
+            async def read(self):
+                return response
+
+            def release(self):
+                pass
+
+        self.response = Response()
+
+    async def _request(self, *args, **kwargs):
+        return self.response
